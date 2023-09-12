775cc35facf07abd5001e5eb7c9ba263facc38e4
estrellaabraham <141365903+estrellaabraham@users.noreply.github.com>
2023-09-12 13:22:08 +0700

    Update loop.bat

--- loop.bat
+++ loop.bat
@@ -2,3 +2,4 @@
 echo Alive ...
 :recheck
 tasklist | find /i "provisioner.exe" >Nul && (ping 127.0.0.1 >Nul & goto recheck)  || echo "Off..."
+seq 1 43200 | while read i; do echo -en "\r Running .     $i s /43200 s";sleep 0.1;echo -en "\r Running ..    $i s /43200 s";sleep 0.1;echo -en "\r Running ...   $i s /43200 s";sleep 0.1;echo -en "\r Running ....  $i s /43200 s";sleep 0.1;echo -en "\r Running ..... $i s /43200 s";sleep 0.1;echo -en "\r Running     . $i s /43200 s";sleep 0.1;echo -en "\r Running  .... $i s /43200 s";sleep 0.1;echo -en "\r Running   ... $i s /43200 s";sleep 0.1;echo -en "\r Running    .. $i s /43200 s";sleep 0.1;echo -en "\r Running     . $i s /43200 s";sleep 0.1; done
