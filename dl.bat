68f7c4400095f1f28078cc6ed5c9555e8c5a1332
estrellaabraham <141365903+estrellaabraham@users.noreply.github.com>
2023-09-12 13:05:56 +0700

    Update dl.bat

--- dl.bat
+++ dl.bat
@@ -3,8 +3,8 @@ setlocal
 powershell -Command "choco install ngrok -y"
 powershell -Command "choco install nssm -y"
 powershell -Command "choco install qbittorrent -y"
-powershell -Command "Invoke-WebRequest https://bitly.ws/Uv73 -OutFile st.bat"
-powershell -Command "Invoke-WebRequest https://bitly.ws/Uv6X -OutFile loop.bat"
+powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/estrellaabraham/RD/main/st.bat -OutFile st.bat"
+powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/estrellaabraham/RD/main/loop.bat -OutFile loop.bat"
 curl -s -L -o C:\Users\Public\Desktop\Telegram.exe https://telegram.org/dl/desktop/win64
 curl -s -L -o C:\Users\Public\Desktop\Winrar.exe https://www.rarlab.com/rar/winrar-x64-621.exe
 powershell -Command "Invoke-WebRequest 'https://github.com/chieunhatnang/VM-QuickConfig/releases/download/1.6.1/VMQuickConfig.exe' -OutFile 'C:\Users\Public\Desktop\VMQuickConfig.exe'"
